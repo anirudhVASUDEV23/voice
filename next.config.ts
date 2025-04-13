fb396a97f1534fa0bf7c4a4996dd707df908ad6e
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-04-14 00:58:11 +0530

    check deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // Ignore ESLint errors during the build process
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Ignore TypeScript errors during the build process
+    ignoreBuildErrors: true,
   },
+  experimental: {},
 };
 
 export default nextConfig;
